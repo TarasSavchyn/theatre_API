7a04740f5f06d6c9437aa9b77d122fa8f19394b0
Taras Savchyn <savik1992@gmail.com>
2023-09-27 21:42:08 +0300

    create models to theatre

--- theatre/models.py
+++ theatre/models.py
@@ -1,3 +1,62 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+class Play(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    genres = models.ManyToManyField(Genre)
+    actors = models.ManyToManyField(Actor)
+
+    def __str__(self):
+        return self.title
+
+class TheatreHall(models.Model):
+    name = models.CharField(max_length=100)
+    rows = models.PositiveIntegerField()
+    seats_in_rows = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
+
+class Performance(models.Model):
+    play = models.ForeignKey(Play, on_delete=models.CASCADE)
+    theatre_hall = models.ForeignKey(TheatreHall, on_delete=models.CASCADE)
+    show_time = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.play.title} at {self.theatre_hall.name}, {self.show_time}"
+
+class Reservation(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    performance = models.ForeignKey(Performance, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Reservation for {self.performance.play.title} by {self.user.username}"
+
+class Ticket(models.Model):
+    row = models.PositiveIntegerField()
+    seat = models.PositiveIntegerField()
+    performance = models.ForeignKey(Performance, on_delete=models.CASCADE)
+    reservation = models.ForeignKey(Reservation, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Ticket for {self.performance.play.title}, Row {self.row}, Seat {self.seat}"
+
