2ed9c75cc736edad453bd6058d28e2001078ea52
Taras Savchyn <savik1992@gmail.com>
2023-09-27 22:41:08 +0300

    add debug_toolbar

--- theatre_service/settings.py
+++ theatre_service/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "debug_toolbar",
     "drf_spectacular",
     "rest_framework_simplejwt",
     "rest_framework.authtoken",
@@ -47,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
