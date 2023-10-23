6a30cb9df5c767cc7678a2cd3c0de300f785c704
Taras Savchyn <savik1992@gmail.com>
2023-10-24 01:29:56 +0300

    add test_rating_api.py

---
+++ theatre/tests/test_rating_api.py
@@ -0,0 +1,40 @@
+from django.test import TestCase
+from user.models import User
+from theatre.models import Rating, Play
+
+
+class RatingCRUDTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(email="test@test.ua", password="test123")
+
+        self.admin = User.objects.create_user(email="admin@test.ua", password="test123")
+
+        self.play = Play.objects.create(
+            title="Test Play", description="Description of the test play"
+        )
+        self.rating = Rating.objects.create(play=self.play, mark=4.5, user=self.user)
+
+    def test_read_rating(self):
+        retrieved_rating = Rating.objects.get(id=self.rating.id)
+        self.assertEqual(retrieved_rating.play, self.play)
+        self.assertEqual(retrieved_rating.mark, 4.5)
+        self.assertEqual(retrieved_rating.user, self.user)
+
+    def test_update_rating(self):
+        self.rating.mark = 3.0
+        self.rating.save()
+        updated_rating = Rating.objects.get(id=self.rating.id)
+        self.assertEqual(updated_rating.mark, 3.0)
+
+    def test_delete_rating(self):
+
+        rating_id = self.rating.id
+        self.rating.delete()
+        with self.assertRaises(Rating.DoesNotExist):
+            Rating.objects.get(id=rating_id)
+
+    def test_create_rating(self):
+        new_rating = Rating.objects.create(play=self.play, mark=3.0, user=self.admin)
+        self.assertEqual(new_rating.play, self.play)
+        self.assertEqual(new_rating.mark, 3.0)
+        self.assertEqual(new_rating.user, self.admin)
